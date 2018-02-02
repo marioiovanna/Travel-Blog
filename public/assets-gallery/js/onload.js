e83213b9032233d1051c5ed469c2276d3469ca65
Mario Iovanna <marioiovanna@hotmail.com>
2018-02-02 08:26:54 -0300

    fix loading imges map

---
+++ public/assets-gallery/js/onload.js
@@ -0,0 +1,10 @@
+//
+// // When windows preloading page
+// $(window).load(function() {
+//
+//         $("#preloading").fadeOut(3500);
+//
+// });
+//
+
+// If want to activate diferent method of LOADING page have to uncomment CSS PRELAODING, erase img in HTML and activate this
